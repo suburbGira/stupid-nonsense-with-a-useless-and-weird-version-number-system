637bdd24853238a4b17ab2950963ce7ad9e082fd
suburbGira <58662333+suburbGira@users.noreply.github.com>
2021-01-11 21:18:47 +0000

    improve randomness
    nice

--- A.py
+++ A.py
@@ -1,9 +1,14 @@
 import random
 
-version = "4 1 10.70063 B 1"
+version = "3 2 3.40784 |3 1"
 
 print("hello gnome this is version number",version,"because fuck")
 
-print("version number syntax: digit one goes down each commit, digit two goes up each commit, digit three is divided by 3.14 every commit except when it would go below zero, then it's multiplied by 4.20, digit four is eight (or another digit pretending to be eight), digit five is the current weekday")
+print("version number syntax: digit one goes down each commit, digit two goes up each commit, digit three is divided by 3.14 every commit except when it would go below zero, then it's multiplied by 4.20, digit four is eight (or another character or group of characters pretending to be or vaguely resembling eight), digit five is the current weekday")
 
-print("here is a random number:",random.randint(1,420),"please cherish it forever")
+
+randomnumber = random.randint(1,420)
+if randomnumber == 420 or if randomnumber == 69:
+  print("your random number is",randomnumber,"nice")
+else:
+  print("here is a random number:",randomnumber,"please cherish it forever")
