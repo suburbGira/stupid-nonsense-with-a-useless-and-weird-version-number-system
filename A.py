1f411df91dbc52d8138fe531edbbb5a91194fcca
suburbGira <58662333+suburbGira@users.noreply.github.com>
2021-01-11 21:13:03 +0000

    hm yes random
    Number

--- A.py
+++ A.py
@@ -1,5 +1,9 @@
-version = "5 0 2.54777 8 1"
+import random
+
+version = "4 1 10.70063 B 1"
 
 print("hello gnome this is version number",version,"because fuck")
 
-print("version number syntax: digit one goes down each commit, digit two goes up each commit, digit three is divided by 3.14 every commit, digit four is eight, digit five is the current weekday")
+print("version number syntax: digit one goes down each commit, digit two goes up each commit, digit three is divided by 3.14 every commit except when it would go below zero, then it's multiplied by 4.20, digit four is eight (or another digit pretending to be eight), digit five is the current weekday")
+
+print("here is a random number:",random.randint(1,420),"please cherish it forever")
