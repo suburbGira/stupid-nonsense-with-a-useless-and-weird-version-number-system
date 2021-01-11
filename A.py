95863420b38f3488a59b04f02956740764f1a58f
suburbGira <58662333+suburbGira@users.noreply.github.com>
2021-01-11 21:07:31 +0000

    haha numbers go A
    why am i doing this

--- A.py
+++ A.py
@@ -1,3 +1,5 @@
-version = "69888"
+version = "5 0 2.54777 8 1"
 
 print("hello gnome this is version number",version,"because fuck")
+
+print("version number syntax: digit one goes down each commit, digit two goes up each commit, digit three is divided by 3.14 every commit, digit four is eight, digit five is the current weekday")
