523bfc1580e291cc10b3ed475e8a342f01311875
suburbGira <58662333+suburbGira@users.noreply.github.com>
2021-01-12 10:23:46 +0000

    new version number rules
    aaaa

--- A.py
+++ A.py
@@ -1,11 +1,11 @@
 import random
 
-version = "2 3 1.08530 E3 1"
+version = "3 2 4.55826 Eight 2"
 
 print("hello gnome this is version number",version,"because fuck")
 
 print("version number syntax: digit one goes down each commit, digit two goes up each commit, digit three is divided by 3.14 every commit except when it would go below zero, then it's multiplied by 4.20, digit four is eight (or another character or group of characters pretending to be or vaguely resembling eight), digit five is the current weekday")
-
+print("version number syntax on tuesdays: digit one goes up each commit, digit two goes down each commit, digit three is multiplied by 4.20 every commit except when it would go above ten, then it's divided by 3.14, digit four is eight (or another character or group of characters pretending to be or vaguely resembling eight), digit five is the current weekday")
 
 randomnumber = random.randint(1,420)
 if randomnumber == 420 or randomnumber == 69:
